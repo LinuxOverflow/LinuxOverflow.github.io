06becdde37f0c24777cb624a982914248c20e45b
Harish <linuxoverflow0@gmail.com>
2025-11-24 16:27:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -167,27 +167,54 @@ if (skillsSection) {
 
 
 
-let section=document.querySelector("section");
-let j=0;
+// let section=document.querySelector("section");
+// let j=0;
 
-while(j<200){
-    let star=document.createElement('i');
-    let x=Math.floor(Math.random()* window.
-    innerWidth);
-    star.style.left=`${x}px`;
-    let y=Math.floor(Math.random()* window.
-    innerHeight);
-    star.style.top=`${y}px`;
+// while(j<200){
+//     let star=document.createElement('i');
+//     let x=Math.floor(Math.random()* window.
+//     innerWidth);
+//     star.style.left=`${x}px`;
+//     let y=Math.floor(Math.random()* window.
+//     innerHeight);
+//     star.style.top=`${y}px`;
 
-    let size=Math.random()*4;
-    star.style.width=`${1+size}px`
-    star.style.height=`${1+size}px`
+//     let size=Math.random()*4;
+//     star.style.width=`${1+size}px`
+//     star.style.height=`${1+size}px`
 
-    let duration =Math.random()*2;
-    star.style.animationDuration=`${duration+1}s`
+//     let duration =Math.random()*2;
+//     star.style.animationDuration=`${duration+1}s`
 
-    section.appendChild(star);
-    j++;
+//     section.appendChild(star);
+//     j++;
+// }
+
+
+// Disable star animation on mobile
+if (window.innerWidth > 768) {
+    let section = document.querySelector("section");
+    let j = 0;
+
+    while (j < 200) {
+        let star = document.createElement('i');
+
+        let x = Math.floor(Math.random() * window.innerWidth);
+        star.style.left = `${x}px`;
+
+        let y = Math.floor(Math.random() * window.innerHeight);
+        star.style.top = `${y}px`;
+
+        let size = Math.random() * 4;
+        star.style.width = `${1 + size}px`;
+        star.style.height = `${1 + size}px`;
+
+        let duration = Math.random() * 2;
+        star.style.animationDuration = `${duration + 1}s`;
+
+        section.appendChild(star);
+        j++;
+    }
 }
 
 
